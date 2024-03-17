836c24ab4093ad55753fb7d266f79df3ae45e65a
Frederik Doll <frederik.doll@carbospacetech.com>
2024-03-17 11:58:37 +0100

    started SpaceCrane

---
+++ SpaceCraneControl/App.xaml.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using SharpWizzDriver;
+using System.Windows;
+using Windows.Devices.Enumeration;
+
+namespace SpaceCraneControl
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private async void Application_Startup(object sender, StartupEventArgs e)
+        {
+            try
+            {
+                var host = Host.CreateDefaultBuilder()
+                      .ConfigureServices(ConfigureSevices).Build();
+
+                var logger = host.Services.GetRequiredService<ILogger<App>>();
+                logger.LogInformation("Host Building Complete");
+
+                var mainWindow = host.Services.GetRequiredService<MainWindow>();
+                mainWindow.Show();
+
+                await host.StartAsync();
+                logger.LogInformation("Waiting For Host Stopping");
+
+                var deviceWatcher = host.Services.GetRequiredService<DeviceWatcher>();
+                deviceWatcher.Start();
+                logger.LogInformation("BLE Watcher started");
+
+                await host.WaitForShutdownAsync();
+                logger.LogInformation("Host Stopped");
+
+                mainWindow.Close();
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Unhandled Exception in Application:{ex.Message}");
+            }
+        }
+
+        void ConfigureSevices(HostBuilderContext context, IServiceCollection services)
+        {
+            services.AddSingleton<BuWizzConnection>();
+            services.AddSingleton<BuWizzState>();
+            services.AddSingleton(s => CreateBLEDeviceWatcher(s));
+            services.AddSingleton<BuWizz>();
+            services.AddSingleton<MainWindow>();
+        }
+
+        private DeviceWatcher CreateBLEDeviceWatcher(IServiceProvider s)
+        {
+            string[] requestedProperties =
+            {
+                "System.Devices.Aep.DeviceAddress",
+                "System.Devices.Aep.IsConnected",
+                "System.Devices.Aep.Bluetooth.Le.IsConnectable"
+            };
+            string aqsAllBluetoothLEDevices = "(System.Devices.Aep.ProtocolId:=\"{bb7bb05e-5972-42b5-94fc-76eaa7084d49}\")";
+
+            var ret = DeviceInformation.CreateWatcher(
+                aqsAllBluetoothLEDevices,
+                requestedProperties,
+                DeviceInformationKind.AssociationEndpoint
+            );
+
+            return ret;
+        }
+    }
+
+}
---
+++ SpaceCraneControl/MainWindow.xaml.cs
@@ -0,0 +1,112 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using SharpWizzDriver;
+using SharpWizzDriver.CallParameters;
+using System.Diagnostics;
+using System.Windows;
+using System.Windows.Media;
+using Windows.Gaming.Input;
+
+namespace SpaceCraneControl
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    [INotifyPropertyChanged]
+    public partial class MainWindow : Window
+    {
+        public BuWizz BuWizz { get; }
+
+        [ObservableProperty]
+        GamepadReading? gamepadReading;
+
+        Gamepad? _gamepad;
+
+        IHostApplicationLifetime _lifetime;
+        bool _closeAccepted = false;
+        ILogger<MainWindow> _logger;
+
+        public MainWindow(
+            ILogger<MainWindow> logger,
+            BuWizz buWizz,
+            IHostApplicationLifetime lifetime
+            )
+        {
+            _logger = logger;
+            _lifetime = lifetime;
+            BuWizz = buWizz;
+            InitializeComponent();
+            DataContext = this;
+
+            Gamepad.GamepadAdded += Gamepad_GamepadAdded;
+            CompositionTarget.Rendering += CompositionTarget_Rendering;
+        }
+
+        Stopwatch _swGamepad = Stopwatch.StartNew();
+        ExtendedMotorDataArgs GamepadMappingSpaceCrane(TimeSpan elapsed)
+        {
+            var t = new ExtendedMotorDataArgs();
+            if (_gamepad is null || GamepadReading is null)
+                return t;
+
+            t.PuMotors[0].TargetValue = (GamepadReading.Value.RightTrigger - GamepadReading.Value.LeftTrigger) * 126;  //Winde
+            t.PuMotors[1].TargetValue = GamepadReading.Value.LeftThumbstickX * 126;  //vorne knicken
+            t.PuMotors[2].TargetValue = GamepadReading.Value.LeftThumbstickY * 126; //Vorne Heben
+            t.PuMotors[3].TargetValue = GamepadReading.Value.RightThumbstickY * 126;  //Gegengewicht
+
+            t.PfMotors[0].TargetValue = 0;
+            t.PfMotors[1].TargetValue = GamepadReading.Value.RightThumbstickX; //Drehen
+
+            _logger.LogInformation($"MotorDataUpdated: {_swGamepad.Elapsed.TotalMilliseconds}");
+            _swGamepad.Restart();
+
+            return t;
+        }
+
+        [RelayCommand]
+        async Task RunGamepadControl()
+        {
+            try
+            {
+                await BuWizz.RunExtendedMotorData(GamepadMappingSpaceCrane, TimeSpan.FromMilliseconds(200), _lifetime.ApplicationStopping);
+            }
+            catch (OperationCanceledException)
+            {
+            }
+        }
+
+        #region callbacks
+        private void CompositionTarget_Rendering(object? sender, EventArgs e)
+        {
+            GamepadReading = _gamepad?.GetCurrentReading();
+
+            if (((GamepadReading?.Buttons ?? GamepadButtons.None) & GamepadButtons.View) == GamepadButtons.View)
+                if (BuWizz.IsMoving)
+                    BuWizz.StopCommand.Execute(null);
+
+            if (((GamepadReading?.Buttons ?? GamepadButtons.None) & GamepadButtons.Menu) == GamepadButtons.Menu)
+                if (!BuWizz.IsMoving)
+                    RunGamepadControlCommand.Execute(null);
+        }
+
+        private void Gamepad_GamepadAdded(object? sender, Gamepad e)
+        {
+            _gamepad = e;
+            _logger.LogInformation($"Gamepad set");
+        }
+
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            if (_closeAccepted)
+                return;
+
+            _closeAccepted = true;
+            e.Cancel = true;
+            _lifetime.StopApplication();
+        }
+        #endregion
+    }
+}
